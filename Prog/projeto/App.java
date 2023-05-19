63b397ae8571df86d43911eab49ca072f91cb73c
Rafael Matesco <matescorapha@gmail.com>
2023-05-19 10:40:41 -0300

    comecando projeto dnv (ksksks)

--- Prog/projeto/App.java
+++ Prog/projeto/App.java
@@ -5,47 +5,30 @@ public class App {
         public static void main(String args[]){
 
                 boolean resp = true;
-                int cont = 0;
+
                 int menu;
 
 
-                Candidata candidatas[] = new Candidata[100];
+                
                 Controle controle = new Controle();
 
                 Scanner entradas = new Scanner(System.in); //scanner de entrada de dados
+
                 while(resp == true){
                         System.out.println("( 1 ) – Incluir\n( 2 ) – Consultar\n( 3 ) – Alterar\n( 4 ) – Excluir\n( 5 ) – Relatar\n( 6 ) – Sair");
                         menu = Integer.parseInt(entradas.next());
                         switch(menu){
                                 case 1:
-                                        candidatas[cont] = new Candidata();
-                                        candidatas[cont] = controle.setCandidata(candidatas[cont]);
-                                        cont++;
+                                        controle.incluirCandidata();
                                         break;
                                 case 2:
-                                        System.out.print("deseja visualizar todas as candidatas ou uma em especifico[2/1]:");
-                                        int respUsuario = Integer.parseInt(entradas.next());
-
-                                        if(respUsuario==1){
-                                                System.out.print("Digite o nome da candidata que deseja visualizar: ");
-                                                String nomeConsult = entradas.next();
-                                                controle.getCandidata(candidatas, nomeConsult);
-                                                break;
-                                        }
-                                        controle.getCandidatas(candidatas);
+                                       controle.consultarCandidata();
                                         break;
                                 case 3:
-                                        System.out.print("Digite o nome da candidata que deseja alterar: ");
-                                        String nomeUpdate = entradas.next();
-                                        candidatas = controle.updateCandidata(candidatas, nomeUpdate);
-
+                                        controle.alterarCandidatas();
                                         break;
                                 case 4:
-                                                
-                                        System.out.print("Digite o nome da candidata que deseja excluir: ");
-                                        String nomeDelete = entradas.next();
-                                        candidatas = controle.deleteCandidata(candidatas, nomeDelete);
-                                        
+                                        controle.excluirCandidatas();
                                         break;
                                 case 5:
 
@@ -57,37 +40,8 @@ public class App {
                                         System.out.println("Digite uma das opcoes corretamente");
                                         break;
                         }
-                        
                 }
-                entradas.close();
-            /*
-             
-            for(int i=0;i<=quant-1;i++){  
-               candidatas[i] = new Candidata(); 
-
-               System.out.print("Nome da candidata:");
-               String nome = entradas.nextLine();
-
-               System.out.print("Nota de Simpatia:");
-               String Simpatiastr = entradas.nextLine();
-               int Simpatia = Integer.parseInt(Simpatiastr);
-
-               System.out.print("Nota de Elegancia:");
-               String Eleganciastr = entradas.nextLine();
-               int Elegancia = Integer.parseInt(Eleganciastr);
-
-               System.out.print("Nota de Beleza:");
-               String Belezastr = entradas.nextLine();
-               int Beleza = Integer.parseInt(Belezastr);
-               
-               candidatas[i].setNome(nome); 
-               candidatas[i].setSimpatia(Simpatia);
-               candidatas[i].setElegancia(Elegancia);
-               candidatas[i].setBeleza(Beleza);
-               System.out.println("");
-             }  */
-             
-             //controle.Impressao(candidatas); //Imprime dados das candidatas
+           entradas.close();
                  
         }
 }
--- Prog/projeto/Controle.java
+++ Prog/projeto/Controle.java
@@ -1,8 +1,42 @@
-
-class Controle {//scanner de entrada de dados
-
+import java.util.Scanner;
+class Controle {
+    Candidata candidatas[] = new Candidata[100];
     ServiceCandidata serviceCandidata = new ServiceCandidata();
+    Scanner entradas = new Scanner(System.in); //scanner de entrada de dados
+    int cont = 0;
 
+    public void incluirCandidata(){
+        candidatas[cont] = new Candidata();
+        candidatas[cont] = setCandidata(candidatas[cont]);
+        cont++;
+    }
+
+    public void consultarCandidata(){
+        System.out.print("deseja visualizar todas as candidatas ou uma em especifico[2/1]:");
+        int respUsuario = Integer.parseInt(entradas.next());
+
+        if(respUsuario==1){
+                System.out.print("Digite o nome da candidata que deseja visualizar: ");
+                String nomeConsult = entradas.next();
+                getCandidata(candidatas, nomeConsult);
+        }
+        getCandidatas(candidatas);
+    }
+
+    public void alterarCandidatas(){
+                System.out.print("Digite o nome da candidata que deseja alterar: ");
+                String nomeUpdate = entradas.next();
+                candidatas = updateCandidata(candidatas, nomeUpdate);
+
+    }
+
+    public void excluirCandidatas(){
+                System.out.print("Digite o nome da candidata que deseja excluir: ");
+                String nomeDelete = entradas.next();
+                candidatas = deleteCandidata(candidatas, nomeDelete);
+    }
+
+ /* */
     public Candidata setCandidata(Candidata dados){ //Seta os dados de uma candidata
             dados = serviceCandidata.setCandidata(dados);
             return dados;
